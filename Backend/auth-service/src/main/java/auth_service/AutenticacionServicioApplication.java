4493477ff5c78e18949b76e2b5b94929dacd4840
marbello1973 <david.marbello@outlook.com>
2025-04-24 20:57:21 -0500

    microservicios

---
+++ Backend/auth-service/src/main/java/auth_service/AutenticacionServicioApplication.java
@@ -0,0 +1,13 @@
+package auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticacionServicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticacionServicioApplication.class, args);
+	}
+
+}
