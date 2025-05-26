4f5a08c19d01324fd19c4c0ce8ace52d2d93a382
Federico Gomez <federicogomezch@gmail.comm>
2025-05-26 14:25:08 -0500

    Reemplaza modelo .keras por .h5 para compatibilidad en Render

--- modelo_inferencia.py
+++ modelo_inferencia.py
@@ -7,7 +7,7 @@ FRAME_SIZE = (64, 64)
 CLASES = list("ABCDEFGHIJ")
 
 # Cargar modelo SOLO una vez
-modelo = load_model("app/modelos/sign_language_model_reducido.h5")
+model = load_model("app/modelos/sign_language_model_reducido.h5")
 
 def predict_sign(frames):
     if len(frames) != FRAMES_POR_VIDEO:
@@ -15,6 +15,6 @@ def predict_sign(frames):
     frames_resized = [cv2.resize(f, FRAME_SIZE) for f in frames]
     frames_np = np.array(frames_resized, dtype=np.float32) / 255.0
     entrada = np.expand_dims(frames_np, axis=0)
-    pred = modelo.predict(entrada)
+    pred = model.predict(entrada)
     idx = np.argmax(pred)
     return CLASES[idx]
